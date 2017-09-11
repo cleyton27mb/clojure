978a8cd1c83053f76fd81a5902888f3890226122
cleyton luiz <cleyton27mb@gmail.com>
2017-09-11 10:25:00 -0400

    Implementado metodos

---
+++ src/forca/core.clj
@@ -0,0 +1,36 @@
+(ns forca.core
+  (:gen-class))
+
+(def total-de-vidas 6)
+(defn perdeu [] (print "Voce Perdeu"))
+(defn ganhou [] print "Voce ganhou")
+
+(defn acertou-a-palavra-toda? [palavra acertos] 
+	(empty? (letras-faltantes palavra acertos)))
+
+(defn letras-faltantes [palavra acertos]
+	(remove (fn [letra] (contains? acertos (str letra))) palavra))
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute)) 
+
+(defn avalia-chute [chute vidas palavra acertos]
+	(if (acertou? chute palavra)
+		(jogo vidas palavra (conj acertos chute))
+		(jogo (dec vidas) palavra acertos)
+		)
+	)
+
+
+(defn jogo [vidas palavra acertos]
+(if(= vidas 0)
+(perdeu)
+(if (acertou-a-palavra-toda? palavra acertos)
+(ganhou)
+(avalia-chute (le-letra!) vidas palavra acertos))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+ 
